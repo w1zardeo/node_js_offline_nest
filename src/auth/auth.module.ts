77c234701d6df92951ce04b9532686cbdc3788ef
Andrii Plodnik <w1zardeot@gmail.com>
2024-05-20 13:44:43 +0300

    Update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,20 @@ import { UsersModule } from 'src/users/types/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport'; 
 import { JwtStrategy } from './jwt.strategy';
+import { config } from 'dotenv';
+import { ConfigEnum } from 'src/common/enums/config.enum';
+import { ConfigService } from '@nestjs/config';
 import { LocalStrategy } from './local.strategy';
+
+config();
+const configService = new ConfigService();
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: '23@_sdf@F!', // Замініть на свій секретний ключ
+      secret: configService.get(ConfigEnum.JWT_SECRET), // Замініть на свій секретний ключ
       signOptions: { expiresIn: '60m' },
     }),
   ],
