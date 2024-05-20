4fcadc83b583f3fe6a6ca99db3ce6ac754ee5b3d
Andrii Plodnik <w1zardeot@gmail.com>
2024-05-20 15:07:19 +0300

    Update

---
+++ ormconfig.ts
@@ -0,0 +1,18 @@
+import {config} from 'dotenv';
+
+config();
+
+module.exports = {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT) || 5432,
+    username: process.env.DATABASE_USER || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'password',
+    database: process.env.DATABASE_NAME || 'andriiplodnik',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    migrations: ['dist/migration/*{.ts,.js}'],
+    cli: {
+      migrationsDir: 'src/migrations',
+    },
+    synchronize: false,
+  };
